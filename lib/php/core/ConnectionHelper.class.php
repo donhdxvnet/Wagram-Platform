21d5452654bad46bcff8a9979b810bacf5e2cdd2
Test <test>
2018-03-14 15:34:59 +0100

    Database: test

--- lib/php/core/ConnectionHelper.class.php
+++ lib/php/core/ConnectionHelper.class.php
@@ -6,7 +6,7 @@
 		private function __construct(){}
 		private function __clone(){}
 		
-		public static function getConnection($dbName = 'rocket')
+		public static function getConnection($dbName = 'test')
 		{	
 			if ( is_null(self::$_instance) || ($dbName != self::$_dbName) )
 			{	
